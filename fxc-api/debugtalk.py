1acfe6a4c98407731c45343b86ff2cb6c07a92db
caiziyang <13128629906@163.com>
2022-03-07 17:40:55 +0800

    提交测试脚本

---
+++ fxc-api/debugtalk.py
@@ -0,0 +1,57 @@
+import time
+from datetime import datetime
+import random
+from httprunner import __version__
+
+
+def get_httprunner_version():
+    return __version__
+
+
+def get_fxm_headers():
+    headers = {"content-type": "application/json",
+               "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
+                             "(KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",}
+    return headers
+
+
+def sleep(n_secs):
+    time.sleep(n_secs)
+
+
+# 获取用户token
+def get_token():
+    token = "2ui0UkpGUF7nWkP025xG25_0257@"
+    return token
+
+
+# 订单号生成
+def order_id():
+    now = datetime.now()
+    ret = ""
+    date_time = now.strftime("%Y%m%d%H%M%S")
+    for i in range(5):
+        num = random.randint(0, 9)
+        ret += "{}".format(num)
+    order_id = "{}{}".format(date_time, ret)
+    return order_id
+
+
+# 下单时间生成
+def order_time():
+    now = datetime.now()
+    order_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    return order_time
+
+
+# 取票码生成
+def voucherCode():
+    voucher_code = ""
+    for i in range(11):
+        num = random.randint(0, 9)
+        voucher_code += "{}".format(num)
+    return voucher_code
+
+
+if __name__ == "__main__":
+    print(get_fxm_headers())
